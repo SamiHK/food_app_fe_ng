1807c6aa5e282b6e2f382f591392963a1661d0a9
G1 <jeevanaawara@gmail.com>
2022-01-15 00:31:36 +0500

    menu page little changing

--- src/app/modules/menu/menu.module.ts
+++ src/app/modules/menu/menu.module.ts
@@ -7,10 +7,12 @@ import { AdminMenuViewComponent } from './components/admin-menu-view/admin-menu-
 import { AdminMenuComponent } from './components/admin-menu/admin-menu.component';
 import { AlertModule, BadgeModule, ButtonModule, CardModule, FormModule, GridModule, ListGroupModule, NavbarModule, NavModule, SidebarModule, SpinnerModule, TableModule } from '@coreui/angular';
 import { IconModule } from '@coreui/icons-angular';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 
 const CORE_UI_MODULES = [
   CardModule, ButtonModule, FormModule, AlertModule, TableModule, SpinnerModule,
-  GridModule, IconModule, BadgeModule, SidebarModule, ListGroupModule
+  GridModule, IconModule, BadgeModule, SidebarModule, ListGroupModule,
+  PerfectScrollbarModule
 ];
 
 @NgModule({
